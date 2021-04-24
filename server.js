3f8fb92477234c7c2e5a0e15406830ded708e964
Michelle Berta <bertafamily@sbcglobal.net>
2021-04-24 12:34:56 -0500

    heroku commit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const path = require("path");
-const db = require("./db/db.json");
+const util = require("util");
+
+const uuid = require("uuid-random");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
@@ -20,15 +25,44 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(db);
+  let db = [];
+  readFile("./db/db.json", "utf8").then((notes) => {
+    console.log(notes, db);
+    try {
+      db = JSON.parse(notes);
+      console.log(db);
+    } catch (error) {
+      console.log("error: " + error);
+      db = [];
+    }
+    return res.json(db);
+  });
 });
 
 app.post("/api/notes", (req, res) => {
-  res.json(db);
+  readFile("./db/db.json", "utf8").then((notes) => {
+    // http://expressjs.com/en/api.html#req.body
+    // https://www.npmjs.com/package/uuid-random#example-usage
+    const currentNotes = JSON.parse(notes);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid(),
+    };
+    currentNotes.push(newNote);
+    writeFile("./db/db.json", JSON.stringify(currentNotes));
+    res.json(200);
+  });
 });
 
-app.delete("/api/notes", (req, res) => {
-  res.json(db);
+app.delete("/api/notes/:id", (req, res) => {
+  readFile("./db/db.json", "utf8").then((notes) => {
+    let currentNotes = JSON.parse(notes);
+    // http://expressjs.com/en/api.html#req.params
+    currentNotes = currentNotes.filter((note) => note.id !== req.params.id);
+    writeFile("./db/db.json", JSON.stringify(currentNotes));
+    res.json(200);
+  });
 });
 
 // // ROUTER
